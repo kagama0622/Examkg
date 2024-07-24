663da89b3d84b8ae54cfea8849df0132b92fda65
kagama0622 <mro2323073@stu.o-hara.ac.jp>
2024-07-24 15:07:47 +0900

    科目

--- src/scoremanager/main/SubjectDeleteAction.java
+++ src/scoremanager/main/SubjectDeleteAction.java
@@ -1,5 +1,7 @@
 package scoremanager.main;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -12,18 +14,3 @@ import tool.Action;
 public class SubjectDeleteAction extends Action{
 
 	public void execute(HttpServletRequest req, HttpServletResponse res)throws Exception {
-
-		HttpSession session = req.getSession();
-		Teacher teacher = (Teacher)session.getAttribute("user");
-
-		String cd = req.getParameter("cd");
-
-		SubjectDao jDao = new SubjectDao();
-		Subject sub = jDao.get(cd, teacher.getSchool());
-
-		req.setAttribute("sub_date", sub);
-
-		req.getRequestDispatcher("subject_delete.jsp").forward(req, res);
-
-	}
-}
--- src/scoremanager/main/SubjectUpdateAction.java
+++ src/scoremanager/main/SubjectUpdateAction.java
@@ -1,29 +1,5 @@
 package scoremanager.main;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
+public class SubjectUpdateAction {
 
-import bean.Subject;
-import bean.Teacher;
-import dao.SubjectDao;
-import tool.Action;
-
-public class SubjectUpdateAction extends Action{
-
-	public void execute(HttpServletRequest req, HttpServletResponse res)throws Exception {
-
-		HttpSession session = req.getSession();
-		Teacher teacher = (Teacher)session.getAttribute("user");
-
-		String cd = req.getParameter("cd");
-
-		SubjectDao jDao = new SubjectDao();
-		Subject sub = jDao.get(cd, teacher.getSchool());
-
-
-		req.setAttribute("sub_date", sub);
-
-		req.getRequestDispatcher("subject_update.jsp").forward(req, res);
-	}
 }
